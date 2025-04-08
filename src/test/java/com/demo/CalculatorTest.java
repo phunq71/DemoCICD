b175240dc98d7956cbf3323c1917cc862c1e6a9d
phunq71 <ngoquocphu71@gmail.com>
2025-04-09 03:50:27 +0700

    first

---
+++ src/test/java/com/demo/CalculatorTest.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CalculatorTest {
+    Calculator calc = new Calculator();
+
+    @Test
+    public void testAdd() {
+        Assert.assertEquals(calc.add(2, 3), 5);
+    }
+
+    @Test
+    public void testSubtract() {
+        Assert.assertEquals(calc.subtract(5, 3), 2);
+    }
+}
