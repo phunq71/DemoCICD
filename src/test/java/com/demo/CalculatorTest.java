519db7334fd425d299f4e4676336d6ba1c658b78
phunq71 <ngoquocphu71@gmail.com>
2025-04-09 04:41:54 +0700

    F

--- src/test/java/com/demo/CalculatorTest.java
+++ src/test/java/com/demo/CalculatorTest.java
@@ -9,7 +9,7 @@ public class CalculatorTest {
 
     @Test
     public void testAdd() {
-        Assert.assertEquals(calc.add(2, 3), 6);
+        Assert.assertEquals(calc.add(2, 3), 5);
     }
 
     @Test
